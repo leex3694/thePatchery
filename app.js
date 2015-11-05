eba7ed7104b4259b845ac81fb0dc662f127da392
Zachary <zturner29@gmail.com>
2015-11-05 14:13:01 -0600

    added Mongo/some passport stuff

--- app.js
+++ app.js
@@ -4,11 +4,30 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
 
+var User = require('./models/user');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
+
+var mongoURI = "mongodb://localhost:27017/thepatchery";
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+MongoDB.on('error', function (err) {
+  console.log('mongodb connection error', err);
+});
+
+MongoDB.once('open', function () {
+  console.log('mongodb connection open');
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'public/views'));
@@ -21,6 +40,46 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public')));
+app.use(session({
+  secret: 'secret',
+  key: 'user',
+  resave: true,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000, secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use('local', new LocalStrategy({passReqToCallback : true, usernameField: 'username'},
+    function(request, username, password, done) {
+      User.findOne({username:username}, function(err, user){
+
+        if(err) throw err;
+        if(!user){
+          return done(null, false, {message: 'Incorrect username or password'});
+        }
+        user.comparePassword(password, function(err, isMatch){
+          if(err) throw err;
+          if(isMatch){
+            return done(null, user);
+          }else{
+            done(null, false, {message: 'Incorrect username or password'});
+          }
+        });
+      });
+    }));
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err,user){
+    if(err) done(err);
+    done(null,user);
+  });
+});
+
 
 app.use('/', routes);
 app.use('/users', users);
---
+++ models/facebookuser.js
@@ -0,0 +1,7 @@
+//module.exports = {
+//
+//    'facebookAuth' : {
+//        'clientID'      : '711298905667165', // your App ID
+//        'clientSecret'  : 'fbf9bc40358b4b2a03ca8c07934a7b63', // your App Secret
+//        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+//    },
