e1f30c1b3e25b3103ce188b4fef111cf8425345f
Erick <lee.erick7@gmail.com>
2015-11-09 13:35:21 -0600

    resolving merge

---
+++ public/javascripts/user/survey.js
@@ -0,0 +1,10 @@
+/**
+ * Created by usuario on 11/9/15.
+ */
+var app = angular.module('UserSurveyApp', []);
+
+app.controller('userSurvey', ['$scope', '$http', function($scope, $http) {
+    alert('hey');
+
+
+}]);
