479c9e951124576126f6beae0a86f82582014f89
Sarah Marrone <smarrone06@gmail.com>
2015-11-12 09:44:17 -0600

    first commit to Heroku

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ MongoDB.once('open', function () {
 app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon1.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
