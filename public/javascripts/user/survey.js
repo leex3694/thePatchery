3490ba7d7210d31edaf68bb5ca913c696843a7c9
Sarah Marrone <smarrone06@gmail.com>
2015-11-16 10:33:34 -0600

    file uploading

--- public/javascripts/user/survey.js
+++ public/javascripts/user/survey.js
@@ -1,40 +1,72 @@
-/**
- * Created by usuario on 11/9/15.
- */
+//SM - need to refactor the name of this file to SurveyController
+
 var app = angular.module('UserSurveyApp', []);
 
-app.controller('userSurvey', ['$scope', '$http', function($scope, $http) {
+app.controller('SurveyController', ['$scope', '$http', 'Upload', function($scope, $http, Upload) {
     $scope.formData = {};
 
-    $scope.submitSurveyForm = function(){
+    $scope.submitSurveyForm = function(event) {
 
         console.log($scope.formData);
 
+        $scope.upload($scope.file);
+    };
 
+    $scope.upload = function (file) {
+        var data = {file: file, formData: $scope.formData};
 
-        var saveSurveyDataObj ={
-            //volunteerId: Schema.Types.ObjectId,      ************** Not sure how this is going to work. its from the schema ***********
-            question1: $scope.volunteerSurveyQ1,
-            question2: $scope.volunteerSurveyQ2,
-            question3: $scope.volunteerSurveyQ3,
-            question4a: $scope.formData.purchaseForm,
-            question4b: $scope.formData.recommended,
-            question5: $scope.volunteerSurveyQ5,
-            question6: $scope.volunteerSurveyQ6
-            //imgFront:
-            //imgBack:
-            //imgSide:
-        };
-
-
-        $http({
-            method: 'POST',
-            url: '/userSurvey/postSurveyResults',
-            data: saveSurveyDataObj
-        })
-
-
+        Upload.upload({
+            url: '/userSurvey/add',
+            data: data
+        }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        });
 
     };
 
+
 }]);
+
+
+
+
+
+
+
+//var app = angular.module('UserSurveyApp', []);
+//
+//app.controller('userSurvey', ['$scope', '$http', function($scope, $http) {
+//    $scope.formData = {};
+//
+//    $scope.submitSurveyForm = function(){
+//
+//        console.log($scope.formData);
+//
+//        var saveSurveyDataObj ={
+//            //volunteerId: Schema.Types.ObjectId,      ************** Not sure how this is going to work. its from the schema ***********
+//            question1: $scope.volunteerSurveyQ1,
+//            question2: $scope.volunteerSurveyQ2,
+//            question3: $scope.volunteerSurveyQ3,
+//            question4a: $scope.formData.purchaseForm,
+//            question4b: $scope.formData.recommended,
+//            question5: $scope.volunteerSurveyQ5,
+//            question6: $scope.volunteerSurveyQ6
+//            //imgFront:
+//            //imgBack:
+//            //imgSide:
+//        };
+//
+//        $http({
+//            method: 'POST',
+//            url: '/userSurvey/postSurveyResults',
+//            data: saveSurveyDataObj
+//        })
+//
+//    };
+//
+//}]);
