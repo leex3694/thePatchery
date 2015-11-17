3bc55db4f02bdbf756197b8ab8eee809b36042b4
Zachary <zturner29@gmail.com>
2015-11-16 19:13:50 -0600

    facebook login goes to heroku

--- app.js
+++ app.js
@@ -86,7 +86,8 @@ passport.use('local', new localStrategy({passReqToCallback : true, usernameField
 passport.use(new FacebookStrategy({
       clientID: 711298905667165,
       clientSecret: 'fbf9bc40358b4b2a03ca8c07934a7b63',
-      callbackURL: "http://localhost:3000/facebookButton/facebook/callback",
+      callbackURL: "https://limitless-cove-3511.herokuapp.com/facebookButton/facebook/callback",
+      //callbackURL: "http://localhost:3000/facebookButton/facebook/callback",
       profileFields: ["emails", "displayName", "name"]
     },
 
