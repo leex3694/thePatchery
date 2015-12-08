bc0a792803a326d4f048d7ba9f5fbdcdaf7d13d5
briandmpls <briandmpls@gmail.com>
2015-12-08 10:17:16 -0600

    refactored setting value of view campaign dropdown menu

--- public/javascripts/ViewCampaignController.js
+++ public/javascripts/ViewCampaignController.js
@@ -26,32 +26,38 @@ app.controller('ViewCampaignController', ['$scope', '$http', function($scope, $h
         });
 
         console.log("Available Options :",$scope.campaignList.availableOptions);
-        //for(var i=0; i < $scope.campaignList.availableOptions.length;i++){
-        //    if($scope.campaignList.availableOptions[i].testers[0].surveyResults != undefined){
-        //        $scope.campaignList.selectedOption = $scope.campaignList.availableOptions[i];
-        //        console.log( $scope.campaignList.selectedOption);
-        //    }
-        //};
 
 
-        //Set to first available option
-        // First option has to have a survey result or app will crash!!!!!!!!!!!!!!!!!!!!!!!!
-        $scope.campaignList.selectedOption = $scope.campaignList.availableOptions[2];
+
+        //Set value of menu to first available option
+        for(var i=0; i < $scope.campaignList.availableOptions.length;i++){
+            if($scope.campaignList.availableOptions[i].testers[0].surveyResults == undefined) {
+                console.log("No survey results");
+            }else{
+                $scope.campaignList.selectedOption = $scope.campaignList.availableOptions[i];
+                console.log( $scope.campaignList.selectedOption);
+            }
+        };
+
 
         function generateData() {
             $scope.testerList = [];
             var tempTester = {};
 
 
-
+            // Loop through the survey results and testers to match results to the right tester
             for(var volIt = 0; volIt < $scope.campaignList.selectedOption.testers[0].volunteer1.length; volIt++) {
                 for (var surIt = 0; surIt < $scope.campaignList.selectedOption.testers[0].volunteer1.length; surIt++) {
 
                     console.log($scope.campaignList.selectedOption.testers[0].volunteer1[volIt].user[0].facebook.id);
-                    //console.log($scope.campaignList.selectedOption.testers[0].surveyResults[surIt].user[0].facebook.id);
 
+
+                    //Testing for survey results
                     if($scope.campaignList.selectedOption.testers[0].surveyResults[surIt] == undefined){
                         console.log("value undefined");
+
+
+                    //If Survey results are found then combine identifier to the results and push to testerList for display
                     }else if ($scope.campaignList.selectedOption.testers[0].volunteer1[volIt].user[0].facebook.id == $scope.campaignList.selectedOption.testers[0].surveyResults[surIt].user[0].facebook.id && $scope.campaignList.selectedOption.testers.length >= 1) {
 
                         var tempTester = {};
